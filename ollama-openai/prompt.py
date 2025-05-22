b98c59765bc8b947566573164bfcca3b3d48c6b2
jmolleda <julio.molleda@email.com>
2025-05-22 10:05:05 +0200

    Refatoring class Prompt

---
+++ ollama-openai/prompt.py
@@ -0,0 +1,6 @@
+class Prompt:
+    default_prompt = (
+        "You are an AI assistant that provides concise and direct answers. "
+        "Answer the following question without explanation or reasoning. "
+        "Give only the specific answer.\n"
+    )
